f9cc351cfcfdcd5cf5819cbf11701b7cb0c81063
tofuburritos <ingvarchick@gmail.com>
2016-06-02 11:16:10 +0300

    signed in

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
-  root 'static_pages#welcome'
+  # root 'static_pages#welcome'
+  root 'sessions#new'
   match '/home',    to: 'static_pages#home',    via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/readapp', to: 'static_pages#readapp', via: 'get'
   match '/formapp', to: 'static_pages#formapp', via: 'get'
   match '/call',    to: 'static_pages#call',    via: 'get'
-  match '/exit',    to: 'static_pages#exit',    via: 'get'
   match '/office',  to: 'static_pages#office',  via: 'get'
-  match '/signin',  to: 'static_pages#signin',  via: 'get'
+  # match '/signin',  to: 'users#signin',  via: 'get'
   match '/signup',  to: 'users#new',  via: 'get'
+  match '/signout',    to: 'sessions#destroy',    via: 'delete'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160601125730) do
+ActiveRecord::Schema.define(version: 20160601163056) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,9 @@ ActiveRecord::Schema.define(version: 20160601125730) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_token"
   end
+
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
